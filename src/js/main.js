3eb1ac3296fbfa1d2e21a02a085c45220e94eafd
Nasirdin <nasirdin.zhaparkulov@gmail.com>
2022-11-22 23:34:41 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,6 @@ const openSecondHeaderModal = (btn1, btn2) => {
   btn2.classList.remove("active");
   btn1.classList.toggle("active");
 };
-const findActivClass = () => {};
 const selectItems = (item1, item2) => {
   item1.addEventListener("click", () => {
     item2.forEach((e) => {
@@ -16,6 +15,10 @@ const selectItems = (item1, item2) => {
     });
     item1.classList.add("active");
   });
+};
+const addActiveBtnDeal = (btn, form) => {
+  btn.classList.toggle("active");
+  form.classList.toggle("active");
 };
 
 secondHeaderBtnArrow.addEventListener("click", () => {
@@ -29,4 +32,31 @@ secondSelectBtnItem.forEach((item) => {
 });
 secondHeaderSecoundItem.forEach((item) => {
   selectItems(item, secondHeaderSecoundItem);
+});
+
+// kanban
+const dealAllMoney = document.getElementById("deal-all-money-new");
+const dealPriceNew = document.querySelectorAll(".deal__price_new");
+
+const orderAmount = () => {
+  let allSum = 0;
+  for (let i = 0; i < dealPriceNew.length; i++) {
+    allSum += dealPriceNew[i].innerText * 1;
+  }
+  return allSum;
+};
+
+dealAllMoney.innerText = `${orderAmount()}`;
+
+const createDealNew = document.getElementById("create-deal-new");
+const createDealNewBtn = document.getElementById("create-deal-new-btn");
+createDealNewBtn.addEventListener("click", () => {
+  addActiveBtnDeal(createDealNewBtn, createDealNew);
+});
+
+const createDealFailed = document.getElementById("create-deal-failed");
+const createDealFailedBtn = document.getElementById("create-deal-failed-btn");
+
+createDealFailedBtn.addEventListener("click", () => {
+  addActiveBtnDeal(createDealFailedBtn, createDealFailed);
 });
